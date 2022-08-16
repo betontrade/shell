ed012e5dc0320ebc7b4bf91a15448ba0f6b90be4
EVGEN <mtmsys4@gmail.com>
2022-08-17 01:12:28 +0300

    New save 1.12

---
+++ hello/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Run!!!");
+
+// hey
+
+Console.WriteLine("Run Run Run!!!");
+Console.WriteLine("Run Run Run!!!");
+Console.WriteLine("Run Run Run!!!");
